d00c6d734d1fe6c03fabc2c2b75ee0adf28cc4a1
Jerry <Jerry@Jerrys-MacBook-Pro.local>
2014-07-25 03:05:33 -0500

    cellready

---
+++ ClubHub/Event.h
@@ -0,0 +1,14 @@
+//
+//  Event.h
+//  ClubHub
+//
+//  Created by Jerry on 7/23/14.
+//  Copyright (c) 2014 JC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Parse/Parse.h>
+
+@interface Event : PFObject
+
+@end
---
+++ ClubHub/School.m
@@ -0,0 +1,13 @@
+//
+//  School.m
+//  ClubHub
+//
+//  Created by Jerry on 7/23/14.
+//  Copyright (c) 2014 JC. All rights reserved.
+//
+
+#import "School.h"
+
+@implementation School
+
+@end
